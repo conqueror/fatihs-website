d737cbd2a2482c7ca3fa926de730c2874caa7573
Fatih Nayebi <f.nayebi@gmail.com>
2025-03-03 00:37:09 -0500

    fix: ensure white text color on Blog page buttons

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -63,7 +63,7 @@
                         
                         <div class="px-6 pb-6 mt-auto">
                             <a href={`/blog/${post.slug}`} 
-                                class="read-more inline-flex items-center px-4 py-2 bg-primary/75 text-white font-bold rounded-lg hover:bg-primary/65 transition-colors duration-300 w-full justify-center">
+                                class="read-more inline-flex items-center px-4 py-2 bg-primary/75 text-white !text-white font-bold rounded-lg hover:bg-primary/65 transition-colors duration-300 w-full justify-center">
                                 <span>Read article</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
@@ -150,14 +150,14 @@
     }
     
     .read-more {
-        color: #3273dc;
+        color: white;
         text-decoration: none;
         font-weight: 500;
         display: inline-block;
     }
     
     .read-more:hover {
-        text-decoration: underline;
+        text-decoration: none;
     }
     
     @media (max-width: 768px) {
